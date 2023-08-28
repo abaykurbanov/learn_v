f00a76cdfd8cbee742340350320e83a3ba5c7715
abaykurbanov <135601742+abaykurbanov@users.noreply.github.com>
2023-08-28 20:47:58 +0600

    button

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,23 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+const counter = ref(0)
+
+const increase =()=>{
+  counter.value++
+}
+const decrease =()=>{
+  counter.value--
+}
 </script>
 
 <template>
-
-</template>
-<div>
-Работает!
+<div class="test">
+{{ counter }}
+<button @click="increase">увеличить</button>
+<button @click="decrease">уменьшить</button>
 </div>
+</template>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
